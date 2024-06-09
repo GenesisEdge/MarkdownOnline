84fc9bf188f049359f036de2213a40478fc1a319
LiWeny16 <a454888395@gmail.com>
2024-06-10 00:30:15 +0800

    😅fix:sync right

--- @cdn/functions/Parser/mdItPlugin/lineNumber.js
+++ @cdn/functions/Parser/mdItPlugin/lineNumber.js
@@ -2,18 +2,31 @@ function markdownitLineNumber(md, option) {
     const injectLineNumbers = function (tokens, idx, options, env, slf) {
         // console.log({ tokens1: tokens })
         let line;
-        if (tokens[idx].map && tokens[idx].level === 0) {
+        // if (tokens[idx].map && tokens[idx].level === 0) {
+        //   line = tokens[idx].map![0]
+        //   // tokens[idx].attrJoin("class", "line")
+        //   tokens[idx].attrSet("data-line", String(line))
+        // } else if (tokens[idx].map && tokens[idx].level === 1) {
+        //   line = tokens[idx].map![0]
+        //   // tokens[idx].attrJoin("class", "line")
+        //   tokens[idx].attrSet("data-line", String(line))
+        //   if (tokens[idx].type === "list_item_open") {
+        //     line = tokens[idx].map![0]
+        //     // tokens[idx].attrJoin("class", "line")
+        //     tokens[idx].attrSet("data-line", String(line))
+        //   }
+        // }
+        if (tokens[idx].map) {
             line = tokens[idx].map[0];
-            // tokens[idx].attrJoin("class", "line")
             tokens[idx].attrSet("data-line", String(line));
         }
-        // console.log(slf)
         return slf.renderToken(tokens, idx, options);
     };
     md.renderer.rules.paragraph_open =
         md.renderer.rules.heading_open =
             md.renderer.rules.table_open =
                 md.renderer.rules.bullet_list_open =
-                    injectLineNumbers;
+                    md.renderer.rules.list_item_open =
+                        injectLineNumbers;
 }
 export { markdownitLineNumber };
